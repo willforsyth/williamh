0288f4d90674c11f87da1e23b8142cc8ca0aa42b
Will Forsyth <will.forsyth@drummondcentral.co.uk>
2015-07-31 14:25:50 +0100

    add in vgrid and test ( not happy about having to lead libraries and the  page ) fork to add to require

--- dev/_assets/scripts/common.js
+++ dev/_assets/scripts/common.js
@@ -1,25 +1,40 @@
 /*jshint unused:false*/
-// var $ = global.window.$ = window.jQuery = require('../../../bower_components/jquery/dist/jquery');
+// var $ = global.window.$ = window.jQuery = require('../../../bower_components/jquery/dist/jquery.min');
 //     encode = require('../../../bower_components/js-md5/js/md5');
 
 // Show Me The Code
 // ----------
+require('../../../bower_components/jquery/dist/jquery');
 require('./components/testPlugin');
 $('body').logWidth();
 
 // Responsive navigation
 // ----------
+require('../../../bower_components/jquery/dist/jquery');
 require('./components/navigation');
 $('.toggle-menu').navigation();
 
 // Secondary navigation
 // ----------
+require('../../../bower_components/jquery/dist/jquery');
 require('./components/secondaryNavigation');
 $('.toggle--sub-menu').secondaryNavigation();
 
-// Masonary grid
+// Masonary grid test
 // -------------
-$('.grid--masonry').masonry({
-  itemSelector: '.grid-item',
-  columnWidth: 200
+// require('../../../bower_components/masonry/dist/masonry.pkgd.min.js');
+// $('.grid--masonry').masonry({
+//   itemSelector: '.grid-item',
+//   columnWidth: 200
+// });
+
+// vgrid
+// -------------------
+require('../../../bower_components/jquery/dist/jquery');
+require('http://blog.xlune.com/2009/09/vgrid/js/jquery.vgrid.min.js');
+$("#grid-content").vgrid({
+	easing: "easeOutQuint",
+	time: 400,
+	delay: 20,
+	wait: 500
 });
